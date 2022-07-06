45e2b855bc7aaa365c9999deb599b38ab062e0dc
bdiazc90 <bdiazc90@gmail.com>
2022-07-05 23:01:22 -0500

    pi trainer v4 with score

--- semana_6/pitrainer/app.js
+++ semana_6/pitrainer/app.js
@@ -22,24 +22,50 @@ button_start.addEventListener('click', function() {
 const pi_decimals = "1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109";
 let position = 0;
 
+let score = 0;
+let attempts = 0;
+let sattempts = 0;
+let fattempts = 0;
+
 const input_pi = document.querySelector("#user_pi");
 const result_pi = document.querySelector("#result_pi");
 
+const h3_attempts = document.querySelector("#h3_attempts");
+const h3_sattempts = document.querySelector("#h3_sattempts");
+const h3_fattempts = document.querySelector("#h3_fattempts");
+
+// KeyDown -> KeyPress -> input value -> KeyUp
+
 input_pi.addEventListener('keyup', function () {
-    const decimal = this.value;
     this.value = "";
+    this.focus();
+});
+
+input_pi.addEventListener('keydown', function (evt) {
+    const decimal = String.fromCharCode(evt.keyCode);
+
+    attempts++;
+    
     // evaluar reglas:
     if (decimal == "" || isNaN(decimal) || decimal != pi_decimals.charAt(position)) {
-        result_pi.style.color = 'red'; 
+        div_game.style.backgroundColor = 'red';
+        fattempts++;
     } else {
         // si pasó las reglas:
-        result_pi.style.color = 'green';
+        div_game.style.backgroundColor = 'green';
         result_pi.innerText += decimal;
         position++;
+        sattempts++;
     }
     // usar timeout para pintarlo de negro:
     setTimeout(function () {
-        result_pi.style.color = 'black';
+        div_game.style.backgroundColor = 'transparent';
     }, 250);
+
+    // SCORE:
+    h3_attempts.querySelector('span').innerText = attempts;
+    h3_sattempts.querySelector('span').innerText = sattempts;
+    h3_fattempts.querySelector('span').innerText = fattempts;
+    h3_score.querySelector('span').innerText = score;
 });
 
