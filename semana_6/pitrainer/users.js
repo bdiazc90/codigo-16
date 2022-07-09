b4ad2fb8cd6cfbd8f79a1bc439c47fd050b4e95b
linder3hs <linderhassinger00@gmail.com>
2022-07-08 21:18:24 -0500

    feat: array of games

--- semana_6/pitrainer/users.js
+++ semana_6/pitrainer/users.js
@@ -46,29 +46,54 @@ button_clear.addEventListener("click", function () {
 });
 
 function createTableHistoric() {
-  const table_history = document.querySelector("#history > table");
+  // const table_history = document.querySelector("#history > table");
+  const container_history = document.querySelector("#history_container");
 
-  users.forEach(function (user) {
-    // Creo un node object llamado ROW
-    const row = document.createElement("tr");
-    const gameover_at = new Date(user.gameover_at);
-    // Personalizo el node:
-    row.innerHTML = `
-        <td>${user.username}</td>
-        
-        `;
+  users.forEach((user) => {
+    // crear los nombres
+    const h4 = document.createElement("h4");
+    h4.innerText = user.username;
 
-    // <td>${user.score.toFixed(2)}</td>
-    // <td>${user.success_attempts}</td>
-    // <td>${gameover_at.toLocaleDateString("es-PE", {
-    //   weekday: "short",
-    //   year: "numeric",
-    //   month: "long",
-    //   day: "numeric",
-    //   hour: "numeric",
-    //   minute: "numeric",
-    // })}</td>
-    // Lo agrega como un hijo de la tabla:
-    table_history.append(row);
+    container_history.append(h4);
+    // crear la table
+    const table = document.createElement("table");
+    const thead = document.createElement("thead");
+
+    thead.innerHTML = `
+		<tr>
+			<th>Score</th>
+			<th>Attempts</th>
+			<th>Success Attempts</th>
+			<th>Failed Attempts</th>
+			<th>Game Over</th>
+		</tr>	
+		`;
+
+    table.append(thead);
+    container_history.append(table);
   });
+
+  // users.forEach(function (user) {
+  //   // Creo un node object llamado ROW
+  //   const row = document.createElement("tr");
+  //   const gameover_at = new Date(user.gameover_at);
+  //   // Personalizo el node:
+  //   row.innerHTML = `
+  //       <td>${user.username}</td>
+
+  //       `;
+
+  // <td>${user.score.toFixed(2)}</td>
+  // <td>${user.success_attempts}</td>
+  // <td>${gameover_at.toLocaleDateString("es-PE", {
+  //   weekday: "short",
+  //   year: "numeric",
+  //   month: "long",
+  //   day: "numeric",
+  //   hour: "numeric",
+  //   minute: "numeric",
+  // })}</td>
+  // Lo agrega como un hijo de la tabla:
+  // table_history.append(row);
+  // });
 }
