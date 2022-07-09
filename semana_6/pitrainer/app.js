7a71e68ac0d8a6f25aad42f3f0a4f85c001c892a
linder3hs <linderhassinger00@gmail.com>
2022-07-08 19:33:45 -0500

    feat: array of games

--- semana_6/pitrainer/app.js
+++ semana_6/pitrainer/app.js
@@ -2,26 +2,24 @@
 const input_username = document.querySelector("#username");
 const button_start = document.querySelector("#start");
 const div_game = document.querySelector("#game");
-let user = null;
+user = null;
 
 input_username.addEventListener("keyup", function () {
-    button_start.disabled = this.value == "";
+  button_start.disabled = this.value == "";
 });
 
 button_start.addEventListener("click", function () {
-    this.parentElement.querySelector(
-        "h2"
-    ).innerText += `Hello ${input_username.value}, let's play`;
-    this.parentElement.querySelector("h2").style.display = "block";
-    
-    // creamos el user:
-    user = new User(input_username.value);
+  this.parentElement.querySelector(
+    "h2"
+  ).innerText += `Hello ${input_username.value}, let's play`;
+  this.parentElement.querySelector("h2").style.display = "block";
 
-    input_username.disabled = true;
-    this.disabled = true;
-    div_game.style.display = "flex";
+  // creamos el user:
+  user = new User(input_username.value);
 
-    
+  input_username.disabled = true;
+  this.disabled = true;
+  div_game.style.display = "flex";
 });
 
 // GAME:
@@ -52,76 +50,83 @@ const btn_reiniciar = document.querySelector("#btn-reiniciar");
 
 // KeyDown -> KeyPress -> input value -> KeyUp
 input_pi.addEventListener("keyup", function () {
-    this.value = "";
-    this.focus();
+  this.value = "";
+  this.focus();
 });
 
 input_pi.addEventListener("keydown", function (evt) {
-    const decimal = String.fromCharCode(evt.keyCode);
+  const decimal = String.fromCharCode(evt.keyCode);
 
-    // el momento en que pongamos un numero
-    let now_attempt = Date.now();
+  // el momento en que pongamos un numero
+  let now_attempt = Date.now();
 
-    attempts++;
+  attempts++;
 
-    // evaluar reglas:
-    if (decimal == "" || isNaN(decimal) || decimal != pi_decimals.charAt(position)) {
-        div_game.style.backgroundColor = "red";
-        failed_attempts++;
-        evalGameOver();
-    } else {
-        // si pasó las reglas:
-        // recuerden que la primera vez position es 0
-        if (position > 0) {
-        calcScore(now_attempt);
-        console.log(score);
-        }
-        div_game.style.backgroundColor = "green";
-        result_pi.innerText += decimal;
-        position++;
-        success_attempts++;
+  // evaluar reglas:
+  if (
+    decimal == "" ||
+    isNaN(decimal) ||
+    decimal != pi_decimals.charAt(position)
+  ) {
+    div_game.style.backgroundColor = "red";
+    failed_attempts++;
+    evalGameOver();
+  } else {
+    // si pasó las reglas:
+    // recuerden que la primera vez position es 0
+    if (position > 0) {
+      calcScore(now_attempt);
+      console.log(score);
     }
+    div_game.style.backgroundColor = "green";
+    result_pi.innerText += decimal;
+    position++;
+    success_attempts++;
+  }
 
-    last_attempt = now_attempt;
-    // usar timeout para pintarlo de negro:
-    setTimeout(function () {
-        div_game.style.backgroundColor = "transparent";
-    }, 250);
+  last_attempt = now_attempt;
+  // usar timeout para pintarlo de negro:
+  setTimeout(function () {
+    div_game.style.backgroundColor = "transparent";
+  }, 250);
 
-    // SCORE:
-    h3_attempts.querySelector("span").innerText = attempts;
-    h3_success_attempts.querySelector("span").innerText = success_attempts;
-    h3_failed_attempts.querySelector("span").innerText = failed_attempts;
-    h3_score.querySelector("span").innerText = score.toFixed(2);
+  // SCORE:
+  h3_attempts.querySelector("span").innerText = attempts;
+  h3_success_attempts.querySelector("span").innerText = success_attempts;
+  h3_failed_attempts.querySelector("span").innerText = failed_attempts;
+  h3_score.querySelector("span").innerText = score.toFixed(2);
 });
 
 function calcScore(now_attempt) {
-    const diff_time = (now_attempt - last_attempt) * 0.01;
-    const attempt_score =
-	position > 10
-	  ? 10 + Number(String(position).charAt(0)) - diff_time
-	  : 10 - diff_time;
-    score += Math.max(attempt_score, -1);
+  const diff_time = (now_attempt - last_attempt) * 0.01;
+  const attempt_score =
+    position > 10
+      ? 10 + Number(String(position).charAt(0)) - diff_time
+      : 10 - diff_time;
+  score += Math.max(attempt_score, -1);
 }
 
 function evalGameOver() {
-    if (failed_attempts >= 10) {
-        // activamos los estilos
-        container_modal.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-        lost_container.style.display = "block";
-        container.style.zIndex = -1;
-        input_pi.disabled = true;
-        h3_score_finish.querySelector("span").innerText = score.toFixed(2);
-        // Crear el usuario y agregarlo a la lista de usuarios:
-        if (user !== null) {
-            user.score = score;
-            user.attempts = attempts;
-            user.success_attempts = success_attempts;
-            user.failed_attempts = failed_attempts;
-            user.gameover();
-            addUserToLocalStorage(user);
-        }
+  if (failed_attempts >= 10) {
+    // activamos los estilos
+    container_modal.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    lost_container.style.display = "block";
+    container.style.zIndex = -1;
+    input_pi.disabled = true;
+    h3_score_finish.querySelector("span").innerText = score.toFixed(2);
+    // Crear el usuario y agregarlo a la lista de usuarios:
+    if (user !== null) {
+      user.games.push({
+        score: score,
+        attempts: attempts,
+        success_attempts: success_attempts,
+        failed_attempts: failed_attempts,
+        gameover_at: user.gameover(),
+      });
+
+      addUserToLocalStorage(user);
     }
+  }
 }
 
 btn_reiniciar.onclick = () => window.location.reload();
--- semana_6/pitrainer/users.js
+++ semana_6/pitrainer/users.js
@@ -1,17 +1,12 @@
 class User {
-	constructor(_username) {
-		this.username = _username;
-		this.created_at = new Date();
-		this.attempts = 0;
-		this.success_attempts = 0;
-		this.failed_attempts = 0;
-		this.score = 0;
-		this.gameover_at = null;
-	}
+  constructor(_username) {
+    this.username = _username;
+    this.games = [];
+  }
 
-	gameover() {
-		this.gameover_at = new Date();
-	}
+  gameover() {
+    return new Date();
+  }
 }
 
 // Arreglo de usuarios:
@@ -19,54 +14,54 @@ let users = [];
 
 // Comprobar si existe el item "pitrainer.users"
 if (localStorage.getItem("pitrainer.users") !== null) {
-	const cadena_texto_users = localStorage.getItem("pitrainer.users");
+  const cadena_texto_users = localStorage.getItem("pitrainer.users");
 
-	users = JSON.parse(cadena_texto_users);
+  users = JSON.parse(cadena_texto_users);
 }
 
 function addUserToLocalStorage(user) {
-	users.push(user);
-	// lo sobreescribo en el LS:
-	localStorage.setItem("pitrainer.users", JSON.stringify(users));
+  users.push(user);
+  // lo sobreescribo en el LS:
+  localStorage.setItem("pitrainer.users", JSON.stringify(users));
 }
 
 const button_clear = document.querySelector("#clear");
 
 if (users.length !== 0) {
-	div_history = document.querySelector("#history");
-	div_history.style.display = "block";
-	createTableHistoric();
-	button_clear.disabled = false;
+  div_history = document.querySelector("#history");
+  div_history.style.display = "block";
+  createTableHistoric();
+  button_clear.disabled = false;
 }
 
 // Botón para reiniciar la tabla de historic:
 button_clear.addEventListener("click", function () {
-	localStorage.removeItem("pitrainer.users");
-	window.location.reload();
+  localStorage.removeItem("pitrainer.users");
+  window.location.reload();
 });
 
 function createTableHistoric() {
-	const table_history = document.querySelector("#history > table");
+  const table_history = document.querySelector("#history > table");
 
-	users.forEach(function (user) {
-		// Creo un node object llamado ROW
-		const row = document.createElement("tr");
-		const gameover_at = new Date(user.gameover_at);
-		// Personalizo el node:
-		row.innerHTML = `
+  users.forEach(function (user) {
+    // Creo un node object llamado ROW
+    const row = document.createElement("tr");
+    const gameover_at = new Date(user.gameover_at);
+    // Personalizo el node:
+    row.innerHTML = `
         <td>${user.username}</td>
         <td>${user.score.toFixed(2)}</td>
         <td>${user.success_attempts}</td>
         <td>${gameover_at.toLocaleDateString("es-PE", {
-			weekday: "short",
-			year: "numeric",
-			month: "long",
-			day: "numeric",
-			hour: "numeric",
-			minute: "numeric",
-		})}</td>
+          weekday: "short",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+          hour: "numeric",
+          minute: "numeric",
+        })}</td>
         `;
-		// Lo agrega como un hijo de la tabla:
-		table_history.append(row);
-	});
+    // Lo agrega como un hijo de la tabla:
+    table_history.append(row);
+  });
 }
