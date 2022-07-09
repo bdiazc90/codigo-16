1f49b529eb10861abc1834fc99b251275a94b4c4
linder3hs <linderhassinger00@gmail.com>
2022-07-09 14:19:12 -0500

    chore: add search by username

--- semana_6/memorygame/app.js
+++ semana_6/memorygame/app.js
@@ -1,53 +1,4 @@
-const cardArray = [
-	{
-		name: "fries",
-		img: "images/fries.png",
-	},
-	{
-		name: "cheeseburger",
-		img: "images/cheeseburger.png",
-	},
-	{
-		name: "hotdog",
-		img: "images/hotdog.png",
-	},
-	{
-		name: "ice-cream",
-		img: "images/ice-cream.png",
-	},
-	{
-		name: "milkshake",
-		img: "images/milkshake.png",
-	},
-	{
-		name: "pizza",
-		img: "images/pizza.png",
-	},
-	{
-		name: "fries",
-		img: "images/fries.png",
-	},
-	{
-		name: "cheeseburger",
-		img: "images/cheeseburger.png",
-	},
-	{
-		name: "hotdog",
-		img: "images/hotdog.png",
-	},
-	{
-		name: "ice-cream",
-		img: "images/ice-cream.png",
-	},
-	{
-		name: "milkshake",
-		img: "images/milkshake.png",
-	},
-	{
-		name: "pizza",
-		img: "images/pizza.png",
-	},
-];
+import { cardArray, flipCard } from "./card.js";
 
 // Barajamos las cartas aleatoriamente:
 cardArray.sort(() => 0.5 - Math.random());
@@ -60,55 +11,55 @@ let cardsChosenId = [];
 let cardsWon = [];
 
 function createBoard() {
-	for (let i = 0; i < cardArray.length; i++) {
-		const card = document.createElement("img");
-		card.setAttribute("src", "images/blank.png");
-		card.setAttribute("data-id", i);
-		card.addEventListener("click", flipCard);
-		gridDisplay.append(card);
-	}
+  for (let i = 0; i < cardArray.length; i++) {
+    const card = document.createElement("img");
+    card.src = "images/blank.png";
+    // card.setAttribute("src", "images/blank.png");
+    card.setAttribute("data-id", i);
+    card.addEventListener("click", function () {
+      flipCard(card, cardsChosen, cardsChosenId, checkForMatch);
+    });
+    gridDisplay.append(card);
+  }
 }
+
 createBoard();
 
 function checkForMatch() {
-	// cards es un array de todas las img renderizadas.
-	const cards = document.querySelectorAll("img");
-	const optionOneId = cardsChosenId[0];
-	const optionTwoId = cardsChosenId[1];
+  // cards es un array de todas las img renderizadas.
+  const cards = document.querySelectorAll("img"); // nodelist
 
-	if (optionOneId === optionTwoId) {
-		// Esta misma card debe cerrarse (blank.png):
-		cards[optionOneId].setAttribute("src", "images/blank.png");
-	} else if (cardsChosen[0] === cardsChosen[1]) {
-		// Queremos colocar ambas cartas como white.png:
-		cards[optionOneId].setAttribute("src", "images/white.png");
-		cards[optionTwoId].setAttribute("src", "images/white.png");
-		// Quitar event click a las cartas white:
-		cards[optionOneId].removeEventListener("click", flipCard);
-		cards[optionTwoId].removeEventListener("click", flipCard);
-		cardsWon.push(cardsChosen);
-	} else {
-		// Queremos volver a cerrar estas 2 cartas.
-		cards[optionOneId].setAttribute("src", "images/blank.png");
-		cards[optionTwoId].setAttribute("src", "images/blank.png");
-	}
+  const optionOneId = cardsChosenId[0];
+  const optionTwoId = cardsChosenId[1];
 
-	cardsChosen = [];
-	cardsChosenId = [];
+  if (optionOneId === optionTwoId) {
+    // Esta misma card debe cerrarse (blank.png):
+    cards[optionOneId].src = "images/blank.jpg";
+  } else if (cardsChosen[0] === cardsChosen[1]) {
+    // Queremos colocar ambas cartas como white.png:
+    cards[optionOneId].src = "images/cup.jpg";
+    cards[optionOneId].style.width = "100px";
+    cards[optionTwoId].src = "images/cup.jpg";
+    cards[optionTwoId].style.width = "100px";
+    // Quitar event click a las cartas white:
+    cards[optionOneId].removeEventListener("click", function () {
+      flipCard(cards[optionOneId], cardsChosen, cardsChosenId, checkForMatch);
+    });
+    cards[optionTwoId].removeEventListener("click", function () {
+      flipCard(cards[optionTwoId], cardsChosen, cardsChosenId, checkForMatch);
+    });
+    cardsWon.push(cardsChosen);
+  } else {
+    // Queremos volver a cerrar estas 2 cartas.
+    cards[optionOneId].src = "images/blank.png";
+    cards[optionTwoId].src = "images/blank.png";
+  }
 
-	resultDisplay.textContent = cardsWon.length;
-	if (cardsWon.length === cardArray.length / 2) {
-		resultDisplay.textContent = "you win";
-	}
-}
+  cardsChosen = [];
+  cardsChosenId = [];
 
-function flipCard() {
-	let cardId = this.getAttribute("data-id");
-	this.setAttribute("src", cardArray[cardId].img);
-	cardsChosen.push(cardArray[cardId].name);
-	cardsChosenId.push(cardId);
-
-	if (cardsChosen.length === 2) {
-		setTimeout(checkForMatch, 100);
-	}
+  resultDisplay.textContent = cardsWon.length;
+  if (cardsWon.length === cardArray.length / 2) {
+    resultDisplay.textContent = "you win";
+  }
 }
