1b5f5c13850ad12fa2cfaa3e70495dcc1ee33d3e
linder3hs <linderhassinger00@gmail.com>
2022-07-15 20:13:02 -0500

    add chart.js

--- semana_7/tasklist/chart-task.js
+++ semana_7/tasklist/chart-task.js
@@ -1,30 +1,57 @@
-const barChart = document.querySelector("#bar-chart").getContext("2d");
+// const barChart = document.querySelector("#bar-chart").getContext("2d");
 
-new Chart(barChart, {
-  type: "bar",
+// new Chart(barChart, {
+//   type: "doughnut",
+//   data: {
+//     labels: [
+//       "Enero",
+//       "Febrero",
+//       "Marzo",
+//       "Abril",
+//       "Mayo",
+//       "Junio",
+//       "Julio",
+//       "Agosto",
+//       "Septiembre",
+//       "Octubre",
+//       "Noviembre",
+//       "Diciembre",
+//     ],
+//     datasets: [
+//       {
+//         label: "Meses del año",
+//         data: [10, 12, 5, 11, 8, 10, 12, 5, 11, 8, 10, 12],
+//         backgroundColor: ["#FF6420", "#20FFA0", "#009EFF", "#DE25FF"],
+//         borderColor: "#c9c9c9", //["#000", "#f01", "#aac", "#019"],
+//         borderWidth: 1,
+//       },
+//     ],
+//   },
+// });
+
+const chartTaskReport = document
+  .querySelector("#task-chart-report")
+  .getContext("2d");
+
+const countTODO = getCountTask("todo");
+const countDONE = getCountTask("done");
+const countDelete = getCountTask("delete");
+
+new Chart(chartTaskReport, {
+  type: "doughnut",
   data: {
-    labels: [
-      "Enero",
-      "Febrero",
-      "Marzo",
-      "Abril",
-      "Mayo",
-      "Junio",
-      "Julio",
-      "Agosto",
-      "Septiembre",
-      "Octubre",
-      "Noviembre",
-      "Diciembre",
-    ],
+    labels: ["TODO", "DONE", "DELETE"],
     datasets: [
       {
-        label: "Meses del año",
-        data: [10, 12, 5, 11, 8, 10, 12, 5, 11, 8, 10, 12],
-        backgroundColor: ["#FF6420", "#20FFA0", "#009EFF", "#DE25FF"],
-        borderColor: "#c9c9c9", //["#000", "#f01", "#aac", "#019"],
-        borderWidth: 1,
+        label: "Estados de tareas",
+        data: [countTODO, countDONE, countDelete],
+        backgroundColor: ["#25A9FF", "#00C45F", "#FF6674"],
       },
     ],
   },
 });
+
+function getCountTask(status) {
+  const tasks = arrayTask.filter((task) => task.status === status);
+  return tasks.length;
+}
