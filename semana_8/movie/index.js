aa4b9144647854651ba00547392367d805749ada
linder3hs <linderhassinger00@gmail.com>
2022-07-18 22:02:09 -0500

    fetch

--- semana_8/movie/index.js
+++ semana_8/movie/index.js
@@ -1,9 +1,35 @@
+const containerMovies = document.querySelector("#container-movies");
+
+function renderCards(movies) {
+  containerMovies.innerHTML = "";
+  movies.forEach((movie) => {
+    
+    containerMovies.innerHTML += `
+        <div class="col-12 col-sm-4 col-md-3 col-lg-3">
+          <div class="card my-3" >
+            <img
+              src="${movie.images["Poster Art"].url}"
+              class="card-img-top"
+              alt=""
+            />
+            <div class="card-body">
+              <div class="card-title">${movie.title}</div>
+              <p class="card-text">
+               ${movie.description}
+              </p>
+            </div>
+          </div>
+        </div>
+    `;
+  });
+}
+
 const url =
   "https://static.rviewer.io/challenges/datasets/dreadful-tomatoes/data.json";
 
 fetch(url)
   .then((respuestita) => respuestita.json())
-  .then((datos) => console.log(datos));
+  .then((datos) => renderCards(datos.entries));
 
 // console.log(peticion);
 // recordemos que fetch retorna una promesa esta tiene 2 escenario
@@ -12,7 +38,7 @@ fetch(url)
 
 //* then() Es un callback, es una funcion que se ejecuta cuando algo terminar
 
-// Ejemplo con el A""PI de pokemon
+// Ejemplo con el API de pokemon
 const urlPokemon = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0";
 
 fetch(urlPokemon)
