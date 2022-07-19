e986b213f8e8c9da6a4bce3848751c4a67367788
linder3hs <linderhassinger00@gmail.com>
2022-07-18 22:53:47 -0500

    fetch

--- semana_8/movie/index.js
+++ semana_8/movie/index.js
@@ -5,13 +5,18 @@ function renderCards(movies) {
   movies.forEach((movie) => {
     containerMovies.innerHTML += `
         <div class="col">
-          <div class="card my-3" >
-            <img
-              src="${movie.images["Poster Art"].url}"
-              class="card-img-top"
-              onerror="this.onerror=null;this.src='https://i.quotev.com/img/q/u/15/12/25/78a31e5f80-imag.jpg';"
-              alt=""
-            />
+          <div class="card my-3">
+            <div class="into-photo">
+              <span class="badge text-bg-${
+                movie.programType === "series" ? "success" : "warning"
+                  }">${movie.programType}</span>
+              <img
+                src="${movie.images["Poster Art"].url}"
+                class="card-img-top"
+                onerror="this.onerror=null;this.src='https://i.quotev.com/img/q/u/15/12/25/78a31e5f80-imag.jpg';"
+                alt=""
+              />
+            </div>
             <div class="card-body">
               <div class="card-title">${movie.title}</div>
               <p class="card-text">
