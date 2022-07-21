f1678c46392d03a4af963609524701a842355946
linder3hs <linderhassinger00@gmail.com>
2022-07-20 20:06:31 -0500

    feat: service

--- semana_8/movie-project/service/index.js
+++ semana_8/movie-project/service/index.js
@@ -1,7 +1,28 @@
 // Estarea los metodos para poder
 /**
- * Obtener
- * Crear
- * Actualizar
- * Eliminar
+ * Obten      => GET
+ * Crear      => POST
+ * Actualizar => PUT
+ * Eliminar   => DELETE
  */
+import { urlMovies, mockAPIUrl } from "./config.js";
+
+/**
+ * Funcion que retorna las 100 peliculas
+ */
+
+const getMovies = async () => {
+  try {
+    const response = await fetch(urlMovies);
+    // tiene 2 key
+    /**
+     * data =>
+     * total: 100
+     * entries: []
+     */
+    const { entries } = await response.json();
+    return entries;
+  } catch (error) {
+    return error;
+  }
+};
