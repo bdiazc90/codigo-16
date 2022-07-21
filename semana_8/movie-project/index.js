9a7e926694992d61a3f83cd1716529c3f0bfc577
linder3hs <linderhassinger00@gmail.com>
2022-07-20 21:58:35 -0500

    feat: only 20

--- semana_8/movie-project/index.js
+++ semana_8/movie-project/index.js
@@ -23,6 +23,11 @@ btnGetMovies.onclick = async function () {
 };
 
 function renderMovie(movie) {
+  // va a contar la cantidad elmentos renderizado en nuestro html
+  const movies = document.querySelectorAll("#container-movies .col");
+
+  if (movies.length === 20) return;
+
   containerMovies.innerHTML += `
     <div class="col">
       <div class="card my-3">
